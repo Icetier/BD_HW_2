90794979156d8cf6fcf1371174c8ed97a32e9bfe
Icetier <150613041+Icetier@users.noreply.github.com>
2024-02-16 18:42:55 +0300

    Update SQL_2.sql

--- SQL_2.sql
+++ SQL_2.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS Staff (
+	id_staff SERIAL PRIMARY KEY,
+	name_staff VARCHAR(80) NOT NULL,
+	name_department VARCHAR(80) NOT NULL,
+	supervisor_id int,
+	FOREIGN KEY (supervisor_id) REFERENCES Staff(id_staff) ON DELETE CASCADE
+);
